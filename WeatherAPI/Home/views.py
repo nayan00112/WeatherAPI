0ff89a98a5aaa6a1b564aa15f67cfd52caa9a4af
Nayan Patel <nayanpatel00112@gmail.com>
2022-10-15 23:56:52 +0530

    Network Error Solve

--- WeatherAPI/Home/views.py
+++ WeatherAPI/Home/views.py
@@ -5,33 +5,36 @@ import requests
 def Home(request):
     if request.method == 'POST':
         address = request.POST['cname']
-        print(address)
         params = {
             'access_key': '31959dc57589631d1487a88e13a8d624',
             'query': address,
         }
 
         # https://weatherstack.com/
-        api_result = requests.get(f'http://api.weatherstack.com/current', params)
-
-        api_response = api_result.json()
-        # requestAPI = api_response['request']
         try:
-            location = api_response['location']
-            current = api_response['current']
-            print(api_response)
-            objects = {
-                # 'requestAPI' : requestAPI,
-                'location': location,
-                'current': current
-            }
+            api_result = requests.get(f'http://api.weatherstack.com/current', params)
+            api_response = api_result.json()
+            try:
+                location = api_response['location']
+                current = api_response['current']
+                print(api_response)
+                objects = {
+                    # 'requestAPI' : requestAPI,
+                    'location': location,
+                    'current': current
+                }
+            except:
+                success = api_response['success']
+                error = api_response['error']
+                objects = {
+                    'success': success,
+                    'error' : error,
+                }
         except:
-            success = api_response['success']
-            error = api_response['error']
             objects = {
-                'success': success,
-                'error' : error,
-            }
+                    'success': "Network Connection Probblem."
+                }
+    
         return render(request, 'Home/index.html', objects)
     else:
         return render(request, 'Home/index.html', {'is_data_false' : 'false'})
